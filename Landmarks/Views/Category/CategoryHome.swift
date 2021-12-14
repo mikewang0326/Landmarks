05f50058e2c298fbc3c27aa2c1fd41a56d853836
MikeWang <hi.from.mikewang@gmail.com>
2021-12-14 21:33:02 +1300

    Add a category view

---
+++ Landmarks/Views/Category/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Mike Wang on 14/12/21.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
