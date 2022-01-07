ba57c2a919aaf73e58f1e5942adea0e06c0b841b
MikeWang <hi.from.mikewang@gmail.com>
2022-01-07 14:36:13 +1300

    Track the page in a SwiftUI View's State

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
+    @Binding var currentPage: Int
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -28,12 +29,12 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [context.coordinator.controllers[0]],
+            [context.coordinator.controllers[currentPage]],
             direction:.forward,
             animated: true)
     }
     
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         var controllers = [UIViewController]()
         
@@ -64,6 +65,14 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             }
             
             return controllers[index + 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            if completed,
+               let visibleViewController = pageViewController.viewControllers?.first,
+               let index = controllers.firstIndex(of: visibleViewController) {
+                parent.currentPage = index
+            }
         }
     }
 }
