312943e0e99ee30b45dbb133ceedfb5b8af353f9
MikeWang <hi.from.mikewang@gmail.com>
2022-01-14 17:16:31 +1300

    Define target WatchLandmarks

---
+++ WatchLandmarks WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Mike Wang on 14/01/22.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
